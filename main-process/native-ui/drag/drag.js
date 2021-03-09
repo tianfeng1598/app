be093bd178bb7a6b1d100c4e7a0189fcc38dfd68
tianfeng1598 <1174742234@qq.com>
2021-03-09 14:48:04 +0800

    translation simplified chinese

--- main-process/native-ui/drag/drag.js
+++ main-process/native-ui/drag/drag.js
@@ -3,6 +3,9 @@ const path = require('path')
 
 ipcMain.on('ondragstart', (event, filepath) => {
   const iconName = 'codeIcon.png'
+  console.log(filepath);
+  // 这个方法就是用来实现拖放的
+  // 对于这个方法，理论上应该也是支持从操作系统拖拽文件到软件中的
   event.sender.startDrag({
     file: filepath,
     icon: path.join(__dirname, iconName)
